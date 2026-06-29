46f04474f4297e5dae65b8090cdf53b6fa0acabd
cky9632@naver.com <cky9632@naver.com>
2026-06-29 18:37:17 +0900

    change domain and key

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.erp119.com"];
+pub const RS_PUB_KEY: &str = "3cT8MIPhveCgXWZvw4i4wPoYEecGvVLGxQV39IFIKFk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
